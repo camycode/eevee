1070ac370a5b9e9c0670558bf79841d667ce26c3
古月 <guyue.me@gmail.com>
2016-04-18 00:17:42 +0800

    编辑系统模型

---
+++ core/Models/System.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Core\Models;
+
+use Illuminate\Support\Facades\Validator;
+
+class System extends Model
+{
+    public function addConfig($key, $value, $source = '')
+    {
+
+    }
+
+    public function updateConfig($key, $value)
+    {
+
+    }
+
+    public function getConfig($key)
+    {
+
+    }
+
+    public function getConfigs($source = null, $prefix = null)
+    {
+
+    }
+
+    public function deleteConfig($key, $value)
+    {
+
+    }
+
+    public function getThemes()
+    {
+
+    }
+
+    public function getPlugins()
+    {
+
+    }
+
+    public function getColorThemes()
+    {
+
+    }
+
+}
