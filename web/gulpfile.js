ade45808147e660b4e62d032ffe91e8b79605039
古月 <guyue.me@gmail.com>
2016-04-17 22:24:11 +0800

    web 测试布局

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -5,42 +5,43 @@ var compass = require('gulp-compass');
 var plumber = require('gulp-plumber');
 
 
-
 // compass
-gulp.task('compass', function() {
+gulp.task('compass', function () {
 
-  return gulp.src('./src/sass/**/*.scss')
-    .pipe(compass({
-      config_file: './config.rb',
-      css: './src/css',
-      sass: './src/sass',
-    }))
-    .on('error', function(error) {
-      this.emit('end');
-      console.log(error);
-    })
-    .pipe(browserSync.stream());
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(compass({
+            config_file: './config.rb',
+            css: './src/css',
+            sass: './src/sass',
+        }))
+        .on('error', function (error) {
+            this.emit('end');
+            console.log(error);
+        })
+        .pipe(browserSync.stream());
+
 });
 
 // browser sync
-gulp.task('browser', function() {
+gulp.task('browser', function () {
 
 
-  watch('./src/sass/**/*.scss', gulp.series('compass'));
+    watch('./src/sass/**/*.scss', gulp.series('compass'));
 
-  watch(['./src/scripts/[^bower]*/**/*.js', './src/scripts/*.js', './src/index.html', './src/views/**/*.html', './src/images/**/*.*'], browserSync.reload);
+    watch(['./src/scripts/[^bower]*/**/*.js', './src/scripts/*.js', './src/index.html', './src/views/**/*.html', './src/images/**/*.*'], browserSync.reload);
 
-  return browserSync.init({
-    server: {
-      baseDir: "./src/"
-    },
-    port: 8080,
-    startPath: '#home'
-      // proxy: "qsq.com/shark/src#home"
-  });
+    return browserSync.init({
+        server: {
+            baseDir: "./src/"
+        },
+        port: 8080,
+        startPath: '#home'
+        // proxy: "qsq.com/shark/src#home"
+    });
+    
 });
 
 // 开启服务
-gulp.task('default', gulp.series('browser'), function(callback) {
-  callback();
+gulp.task('default', gulp.series('browser'), function (callback) {
+    callback();
 });
