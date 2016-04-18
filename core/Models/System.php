302a7ac33feca450979a2f4ce425f2bd5f803495
古月 <guyue.me@gmail.com>
2016-04-18 09:10:18 +0800

    编辑系统模型

--- core/Models/System.php
+++ core/Models/System.php
@@ -6,19 +6,66 @@ use Illuminate\Support\Facades\Validator;
 
 class System extends Model
 {
+    /**
+     * 添加系统设置
+     *
+     * @param $key
+     * @param $value
+     * @param string $source
+     *
+     * @return mixed
+     *
+     * @throws \Core\Exceptions\StatusException
+     */
     public function addConfig($key, $value, $source = '')
     {
+        if ($this->resource('CONFIG')->insert(['config_key' => $key, 'config_value' => $value, 'source' => $source])) {
 
+            return $this->getConfig($key);
+        }
+
+        exception('addConfigFailed');
     }
 
+    /**
+     * 更新系统设置
+     *
+     * @param $key
+     * @param $value
+     *
+     * @return mixed
+     *
+     * @throws \Core\Exceptions\StatusException
+     */
     public function updateConfig($key, $value)
     {
+        $this->getConfig($key);
 
+        if ($this->resource('CONFIG')->where('config_key', $key)->update(['config_value' => $value])) {
+
+            return $this->getConfig($key);
+        }
+
+        exception('updateConifgFailed');
     }
 
+    /**
+     * 获取系统设置
+     *
+     * @param $key
+     *
+     * @return mixed
+     *
+     * @throws \Core\Exceptions\StatusException
+     */
     public function getConfig($key)
     {
+        if ($config = $this->resource('CONFIG')->where('config_key', $key)->first()) {
 
+            return status('success', $config);
+        }
+
+        exception('configDoesNotExist');
     }
 
     public function getConfigs($source = null, $prefix = null)
@@ -45,5 +92,6 @@ class System extends Model
     {
 
     }
+
 
 }
