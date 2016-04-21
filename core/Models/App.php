a7c669ff87d2fbee89dd02b3466689e7fe39fb9b
古月 <guyue.me@gmail.com>
2016-04-22 00:53:09 +0800

    开发APP模型

--- core/Models/App.php
+++ core/Models/App.php
@@ -6,42 +6,112 @@ use Illuminate\Support\Facades\Validator;
 
 class APP extends Model
 {
-    protected $data;
+    protected $data = [];
 
     public function setData($data)
     {
         $this->data = $data;
     }
 
+    /**
+     * 添加APP
+     *
+     * @return Status
+     */
     public function addApp()
     {
+        $this->validateApp();
+
+        $this->filter($this->data, $this->fields('APP'));
+
+        $this->resource('APP')->insert($this->data);
+
+        return $this->getApp($this->data['id']);
 
     }
 
+    /**
+     * 更新APP
+     *
+     * @param string $app_id
+     */
     public function updateApp($app_id)
     {
+        $app = $this->getApp($app_id);
 
+        $ignore = ['id'];
+
+        if ($app->name == $this->data['name']) {
+
+            array_push($ignore, 'name');
+        };
+
+        $this->validateApp($ignore);
+
+        $this->filter($this->data, $this->fields('APP'));
+
+        $this->resource('APP')->where('id', $app_id)->update($this->data);
     }
 
+    /**
+     * 获取APP
+     *
+     * @param $app_id
+     *
+     * @return Status
+     */
     public function getApp($app_id)
     {
 
+        if ($app = $this->resource('APP')->where('id', $app_id)->first()) {
+
+            return status('success', $app);
+        }
+
+        return status('appDoesNotExist');
     }
 
+    /**
+     * 获取APP组
+     *
+     * @param array $params
+     *
+     * @return Status
+     */
     public function getApps(array $params = [])
     {
-
+        return status('success', $this->selector('APP', $params));
     }
 
+    /**
+     * 删除APP
+     *
+     * @param string $app_id
+     *
+     * @return Status
+     */
     public function deleteApp($app_id)
     {
+        if ($this->resource('APP')->where('id', $app_id)->delete()) {
 
+            return status('success');
+        }
+
+        return status('appDoesNotExist');
     }
 
-    protected function validateApp()
+    /**
+     * 验证APP
+     *
+     * @param array $ignore
+     *
+     * @throws \Core\Exceptions\StatusException
+     */
+    protected function validateApp(array $ignore = [])
     {
 
         $table = $this->table('APP');
+
 
         $rule = [
             'app_id' => "required|unique:$table",
@@ -49,13 +119,16 @@ class APP extends Model
         ];
 
 
+        $this->ignore($rule, $ignore);
+
+
         $validator = Validator::make($this->data, $rule);
 
         if ($validator->fails()) {
 
-            return exception('validateFailed', $validator->errors());
+            exception('validateFailed', $validator->errors());
         }
-        
+
     }
 
 }
