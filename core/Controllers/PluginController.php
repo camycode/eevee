6bcf22b2b877be1c62c8c82535af7da5286c5b86
古月 <guyue.me@gmail.com>
2016-04-24 23:32:37 +0800

    编辑插件控制器

---
+++ core/Controllers/PluginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Core\Controllers;
+
+use Core\Services\Context;
+use Illuminate\Support\Facades\Storage;
+
+class PluginController extends Controller
+{
+
+
+    public function getPlugins(Context $context)
+    {
+
+    }
+
+    public function getPlugin(Context $context)
+    {
+
+    }
+
+    public function deletePlugin(Context $context)
+    {
+
+    }
+
+    public function install(Context $context)
+    {
+
+    }
+
+    public function uninstall(Context $context)
+    {
+
+    }
+    
+}
