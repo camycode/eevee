54b26e8c49e7ad0876346f375b3061f2d75e9248
古月 <guyue.me@gmail.com>
2016-05-18 11:41:45 +0800

    添加角色管理菜单权限绑定

--- core/System/config/menus.php
+++ core/System/config/menus.php
@@ -7,7 +7,7 @@ return [
     ['name' => trans_name('resources.TERM'),                'icon' => 'block layout icon',  'icon_url' => false, 'link' => '#app/term',         'permission' => 'TERM_GET'],
 //    ['name' => trans_name('resources.MEDIA'),               'icon' => 'video play icon',    'icon_url' => false, 'link' => '#app/media',        'permission' => false],
     ['name' => trans_name('resources.USER'),                'icon' => 'user icon',          'icon_url' => false, 'link' => '#app/user',         'permission' => 'USER_GET'],
-    ['name' => trans_name('resources.ROLE'),                'icon' => 'spy icon',           'icon_url' => false, 'link' => '#app/role',         'permission' => false],
+    ['name' => trans_name('resources.ROLE'),                'icon' => 'spy icon',           'icon_url' => false, 'link' => '#app/role',         'permission' => 'ROLE_GET'],
 //    ['name' => trans_name('resources.THEME'),               'icon' => 'paint brush icon',   'icon_url' => false, 'link' => '#app/theme',        'permission' => false],
 //    ['name' => trans_name('resources.PLUGIN'),              'icon' => 'puzzle icon',        'icon_url' => false, 'link' => '#app/plugin',       'permission' => false],
     ['name' => trans_name('resources.SYSTEM_CONFIG'),       'icon' => 'setting icon',       'icon_url' => false, 'link' => false,               'permission' => 'SYSTEM_CONFIG'],
