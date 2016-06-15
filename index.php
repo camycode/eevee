14b86b9e1d80b3f6582f0815381e330b4b1fadbe
古月 <guyue.me@gmail.com>
2016-06-15 17:14:30 +0800

    编辑入口文件

--- index.php
+++ index.php
@@ -12,16 +12,6 @@
 if (!file_exists('install.lock')) {
 }
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| First we need to get an application instance. This creates an instance
-| of the application / container and bootstraps the application so it
-| is ready to receive HTTP / Console requests from the environment.
-|
-*/
 
 require_once __DIR__ . '/vendor/autoload.php';
 
