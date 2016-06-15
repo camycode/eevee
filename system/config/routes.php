66d5828bfd823bc8c99fe55030adb491070187b5
古月 <guyue.me@gmail.com>
2016-06-15 17:36:53 +0800

    添加路由文件

--- system/config/routes.php
+++ system/config/routes.php
@@ -16,32 +16,32 @@ return [
 
 
     // 用户模块
-    'post@user'   => ['action' => 'UserController@postUser',    'permission' => 'USER_POST'],
-    'get@users'   => ['action' => 'UserController@getUsers',    'permission' => 'USER_GET'],
-    'get@user'    => ['action' => 'UserController@getUser',     'permission' => 'USER_GET'],
-    'put@user'    => ['action' => 'UserController@putUser',     'permission' => 'USER_PUT'],
-    'delete@user' => ['action' => 'UserController@deleteUser',  'permission' => 'USER_DELETE'],
+    'post@user'   => ['action' => 'UserController@postUser',    ],
+    'get@users'   => ['action' => 'UserController@getUsers',    ],
+    'get@user'    => ['action' => 'UserController@getUser',     ],
+    'put@user'    => ['action' => 'UserController@putUser',     ],
+    'delete@user' => ['action' => 'UserController@deleteUser',  ],
 
     // 角色模块
-    'post@role'             => ['action' => 'RoleController@postRole',          'permission' => 'ROLE_POST'],
-    'put@role'              => ['action' => 'RoleController@putRole',           'permission' => 'ROLE_PUT'],
-    'delete@role'           => ['action' => 'RoleController@deleteRole',        'permission' => 'ROLE_DELETE'],
-    'get@roles'             => ['action' => 'RoleController@getRoles',          'permission' => ['ROLE_GET','ROLE_GET']],
-    'get@role'              => ['action' => 'RoleController@getRole',           'permission' => 'ROLE_GET'],
-    'get@role/permissions'  => ['action' => 'RoleController@getPermissions',    'permission' => 'ROLE_GET'],
+    'post@role'             => ['action' => 'RoleController@postRole',       ß],
+    'put@role'              => ['action' => 'RoleController@putRole',        ß],
+    'delete@role'           => ['action' => 'RoleController@deleteRole',     ß],
+    'get@roles'             => ['action' => 'RoleController@getRoles',       ß],
+    'get@role'              => ['action' => 'RoleController@getRole',        ß],
+    'get@role/permissions'  => ['action' => 'RoleController@getPermissions', ß],
 
     // 系统模块
     'get@version'           => ['action' => 'SystemController@version'],
     'post@system/install'   => ['action' => 'SystemController@install'],
     'get@asset'             => ['action' => 'SystemController@asset'],
-    'post@system/config'    => ['action' => 'SystemController@postConfig',      'permission' => 'SYSTEM_CONFIG_POST'],
-    'put@system/config'     => ['action' => 'SystemController@putConfig',       'permission' => 'SYSTEM_CONFIG_PUT'],
-    'get@system/config'     => ['action' => 'SystemController@getConfig',       'permission' => 'SYSTEM_CONFIG_GET'],
-    'delete@system/config'  => ['action' => 'SystemController@deleteConfig',    'permission' => 'SYSTEM_CONFIG_DELETE'],
+    'post@system/config'    => ['action' => 'SystemController@postConfig',      ],
+    'put@system/config'     => ['action' => 'SystemController@putConfig',       ],
+    'get@system/config'     => ['action' => 'SystemController@getConfig',       ],
+    'delete@system/config'  => ['action' => 'SystemController@deleteConfig',    ],
     'get@system/user/menu'  => ['action' => 'SystemController@getUserMenu'],
 
     // 插件模块
-    'get@local/plugins' => ['action' => 'PluginController@getLocalPlugins', 'permission' => 'USER_GET'],
+    'get@local/plugins' => ['action' => 'PluginController@getLocalPlugins', ],
 
     // 用户设置
     'post@config'       => ['action' => 'ConfigController@postConfig'],
