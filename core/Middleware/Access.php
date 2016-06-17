01e2b1e9a861f252dfae6f71fc80e0cdf98e1c19
古月 <guyue.me@gmail.com>
2016-06-17 18:25:43 +0800

    编辑访问控制中间件

--- core/Middleware/Access.php
+++ core/Middleware/Access.php
@@ -8,7 +8,51 @@ use Core\Logs\AccessLog;
 class Access
 {
 
+    /**
+     * 访问日志
+     *
+     * @var array
+     */
     protected $access_log = [];
+
+    /**
+     * 关键隐藏字段
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * 获取请求数据
+     *
+     * @param $request
+     *
+     * @return mixed
+     */
+    protected function getRequestData($request)
+    {
+        $data = $request->json()->all();
+
+        foreach ($this->hidden as $item) {
+
+            if (isset($data[$item])) {
+
+                $data[$item] = '***';
+            }
+        }
+
+        return json_encode($data);
+    }
+
+    /**
+     * 保存访问日志
+     */
+    protected function saveAccessLogRecord()
+    {
+        AccessLog::create($this->access_log);
+    }
 
     /**
      * 访问控制中间件
@@ -18,7 +62,7 @@ class Access
 
         $this->access_log['method'] = $request->method();
         $this->access_log['uri'] = $request->path();
-        $this->access_log['request_params'] = json_encode($request->query());
+        $this->access_log['request_params'] = $this->getRequestData($request);
         $this->access_log['request_data'] = json_encode($request->json()->all());
         $this->access_log['ip'] = $request->ip();
         $this->access_log['access_begin_at'] = date('Y-m-d H:i:s');
@@ -33,7 +77,7 @@ class Access
 
             $this->access_log['status_code'] = $status->code;
             $this->access_log['status_message'] = $status->message;
-            $this->access_log['status_data'] = $status->data;
+            $this->access_log['status_data'] = json_encode($status->data);
 
         } catch (\Exception $e) {
 
@@ -43,7 +87,9 @@ class Access
 
         }
 
-        AccessLog::create($this->access_log);
+        $this->access_log['access_end_at'] = date('Y-m-d H:i:s');
+
+        $this->saveAccessLogRecord();
 
         return $response;
     }
