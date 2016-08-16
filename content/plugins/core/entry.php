80062c9d3333b2d982128ed456fd8e45ca7320d4
古月 <guyue.me@gmail.com>
2016-08-16 19:22:08 +0800

    编辑插件示例

--- content/plugins/core/entry.php
+++ content/plugins/core/entry.php
@@ -1,34 +1,19 @@
 <?php
 
-use Core\Services\Http;
 use Core\Services\Context;
 use Illuminate\Support\Facades\DB;
 
 global $app;
 
+
 $app->get('/', function (Context $context) {
 
-   $content = Http::request('/api/core/hello','GET');
-
-//    print_r($content);
-
-
-//    echo 'Hello';
-//    return $context->response(status('success', DB::table('app')->get()));
-
-//    echo 'Hello Core';
+    echo 'Hello Core';
 });
+
 
 $app->get('/hello', function (Context $context) {
 
     return $context->status('success', DB::table('app')->get());
-
-});
-
-
-
-add_action('load_plugin_styles', function () {
-
-//    echo 'Hello world';
 
 });
