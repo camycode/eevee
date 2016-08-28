5db1bf568596f7b0ac6759a1b70b81e95e9abcf0
古月 <guyue.me@gmail.com>
2016-08-28 13:02:56 +0800

    删除远程文件

---
+++ content/plugins/account_book/entry.php
@@ -0,0 +1,66 @@
+<?php
+
+
+global $app;
+
+use Core\Services\Context;
+
+/**
+ * 获取账单字段
+ *
+ * @return array
+ */
+function get_account_book_bill_fields()
+{
+
+    return ['id', 'user_id', 'sum', 'type', 'notes', 'created_at', 'updated_at'];
+}
+
+/**
+ * 验证账本账单
+ *
+ * @param array $data
+ *
+ * @return mixed
+ */
+function validate_account_book_bill(array $data)
+{
+
+    $rule = [
+        'user_id' => 'required',
+        'sum' => 'required',
+    ];
+
+    $message = [
+        'sum.required' => '金额不能为空',
+    ];
+
+    return validate($data, $rule, $message);
+
+}
+
+
+$app->post('/bill', function (Context $context) {
+
+    $data = $context->data();
+
+    if ($result = validate_account_book_bill($data) !== true) {
+
+        exception('ValidateError', $result);
+    };
+
+    filter_fields($data, get_account_book_bill_fields());
+
+    $data['id'] = id();
+    $data['created_at'] = timestamp();
+
+    if (table('account_book_bills')->insert($data)) {
+
+        return $context->status('success');
+    } else {
+
+        return $context->status('error');
+    }
+
+
+});
