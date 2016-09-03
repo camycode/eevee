3191a2562bc9b029e97fcbc2995d68503740b0bb
RT7oney <297085213@qq.com>
2016-09-03 18:10:21 +0800

    test wechat plugin

--- content/plugins/wechat/entry.php
+++ content/plugins/wechat/entry.php
@@ -2,21 +2,45 @@
 
 add_action('load_side_menus', function () {
 
-    load_side_menu(
-        [
-            'name' => '微信公众平台',
-            'icon' => 'icon file',
-            'link' => 'javascript:;'
-        ],
-        [
-            [
-                'name' => '用户统计',
-                'link' => ''
-            ],
-            [
-                'name' => '设置',
-                'link' => ''
-            ]
-        ]);
+	load_side_menu(
+		[
+			'name' => '微信公众平台',
+			'icon' => 'icon file',
+			'link' => 'javascript:;',
+		],
+		[
+			[
+				'name' => '用户统计',
+				'link' => '',
+			],
+			[
+				'name' => '设置',
+				'link' => '',
+			],
+		]);
 });
 
+set_connection('ximu_wechat', [
+	'driver' => 'mysql',
+	'host' => env('DB_HOST', 'localhost'),
+	'port' => env('DB_PORT', 3306),
+	'database' => 'ximuop',
+	'username' => env('DB_USERNAME', 'forge'),
+	'password' => env('DB_PASSWORD', ''),
+	'charset' => 'utf8',
+	'collation' => 'utf8_unicode_ci',
+	'prefix' => 'xi_',
+	'timezone' => env('DB_TIMEZONE', '+00:00'),
+	'strict' => false,
+]);
+
+global $app;
+
+use Core\Services\Context;
+
+$app->get('/test', function (Context $context) {
+
+	return $context->status('success', [
+		"welcome" => 'Hello world!',
+	]);
+});
